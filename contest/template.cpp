e93a6c58ea05a84211fb809583e388d63c029c2d
Michael Nematollahi <m.nematollahi99@gmail.com>
2020-05-19 16:38:18 -0400

    Added a contest template.
    Modified gitignore to include swp files.

---
+++ contest/template.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define F first
+#define S second
+#define sz(x) (int)(x).size()
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin.exceptions(cin.failbit);
+	return 0;
+}
