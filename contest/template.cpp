8a7ad9e72425196c0b9a81af5debd104e672af36
Victor <platers81@gmail.com>
2020-05-19 17:03:17 -0400

    Added macros to template

--- contest/template.cpp
+++ contest/template.cpp
@@ -5,9 +5,11 @@ using namespace std;
 #define F first
 #define S second
 #define sz(x) (int)(x).size()
+#define pb push_back
 
 typedef long long ll;
 typedef pair<int, int> pii;
+typedef vector<int> vi;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
